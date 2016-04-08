207f76698cff3fe10f7077e35e79188501f6715b
Haney Family Computer <robert.a.haney@gmail.com>
2016-04-08 11:58:28 -0400

    type: feat
    -Added Backbone Collection
    -Split view into single item and application level components
    -Added local storage

--- app.js
+++ app.js
@@ -1,48 +1,59 @@
 var app = {};
+    
+    app.FoodItem = Backbone.Model.extend({
+      defaults: {
+        title: '',
+        calories: 0
+      }
+    });
+    
+    app.FoodList = Backbone.Collection.extend({
+      model: app.FoodItem,
+      localStorage: new Store("backbone-food")
+    });
 
-app.Foods = Backbone.Model.extend({
-	initialize: function() {
-		console.log("Model initialized");
-		//set listener for model changes
-		this.on('change', function() {
-			console.log('Model values changed');
-		});
-	}, 
-	defaults: {
-		name: '',
-		calories: 0
-	}
-});
+    app.FoodItemView = Backbone.View.extend({
+      tagname: 'li',
+      template: _.template($('#item-template').html()),
+      render: function() {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this; 
+      }
+    });
 
-app.AppView = Backbone.View.extend({
-	el: $('#results'),
-	initialize: function() {
-		console.log("View initialized");
-		_.bindAll(this, 'render');
-		this.listenTo(this.model, 'change', this.render);
-		this.render();
-	},
-	events: {
-		'keypress #fooditem': 'updateOnEnter'
-	},
-	render: function() {
-		$(this.el).html("<div class='food-result'><strong>Food item:</strong> " + this.model.get('name') + "</div>");
-		$(this.el).append("<div class='food-result'><strong>Calories:</strong> " + this.model.get('calories') + "</div>");
-	},
-	updateOnEnter: function(e) {
-		if (e.which === 13) {
-			var value = $('#fooditem').val().trim();
-			this.model.set("name", value);
-		}
-	}
-});
+    app.AppView = Backbone.View.extend({
+      el: '#healthapp',
+      initialize: function() {
+        this.input = this.$('#fooditem');
+        app.FoodList.on('add', this.addOne, this); //render with addOne when new elements added to collection
+        app.FoodList.fetch(); //load list from local storage
+      },
+      events: {
+        'keypress #fooditem': 'updateOnEnter'
+      },
+      updateOnEnter: function(e) {
+        //check if keypress not enter or input is blank
+        if (e.which !== 13 || !this.input.val().trim()) {
+          return;
+        }
+        //otherwise create new instance of a model within the collection
+        app.FoodList.create(this.newAttributes());
+        this.input.val(''); //erase input field
+      },
+      addOne: function(fooditem) {
+        //create individual model view for food item and append result of render to list
+        var view = new app.FoodItemView({model: fooditem});
+        $('#food-list').append(view.render().el);
+      },
+      //currently only returns value of input for food item along with default calories value
+      newAttributes: function() {
+        return {
+          title: this.input.val().trim(),
+          calories: 0
+        }
+      }
+    });
 
-var myFood = new app.Foods({name: 'cake', calories: 500});
-var appView = new app.AppView({model: myFood});
-//check attributes of Foods instance
-console.log(myFood.attributes);
-//change attributes test
-myFood.set('name', 'cookie');
-console.log(myFood.attributes);
-
-
+  //instantiate
+  app.FoodList = new app.FoodList();
+  app.appView = new app.AppView();
